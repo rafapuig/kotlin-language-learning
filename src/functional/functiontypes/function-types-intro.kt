58a53b7ebf65bf6907c07fcf803b0eb7a1a65432
Rafa Puig <rafapuig.profe@gmail.com>
2025-09-22 16:16:44 +0200

    high order functions

--- src/functional/functiontypes/function-types-intro.kt
+++ src/functional/functiontypes/function-types-intro.kt
@@ -1,5 +1,11 @@
 package functional.functiontypes
 
+/**
+ * El tipo Unit se usa para especificar que una función no devuelve un valor
+ * Se puede omitir en la declaración de una función regular
+ * Pero en una declaración de tipo de función NO se puede omitir
+ * porque siempre hay que especificar el tipo de retorno
+ */
 fun explicitLambdaParameterTypesDeclaration() {
     val sum: (Int, Int) -> Int = { x: Int, y: Int -> x + y }
     val action: () -> Unit = { println("Hola lambdas") }
@@ -9,7 +15,7 @@ fun explicitLambdaParameterTypesDeclaration() {
 /**
  * Se pueden omitir los tipos de los parámetros de entrada de la expresión lambda
  * si están especificados en el tipo de la función
- * el la declaración explicita del tipo de dato de la variable
+ * en la declaración explicita del tipo de dato de la variable que almacena la lambda
  */
 fun implicitLambdaParameterTypesDeclaration() {
     val sum: (Int, Int) -> Int = { x, y -> x + y }
@@ -17,15 +23,25 @@ fun implicitLambdaParameterTypesDeclaration() {
     val isPositive: (Int) -> Boolean = { x -> x > 0 }
 }
 
-fun xxxx() {
+fun nullability() {
+
+    /**
+     * Anulabilidad del tipo de retorno en el tipo de función
+     */
     // El tipo de datos de la variable es una función que recibe 2 Int y que puede devolver un Int? (un Int o null)
     var canReturnNull: (Int, Int) -> Int? = { x, y -> if (x == y) x else null }
     // canReturnNull = null // error, la variable no puede contener el valor null
 
+    /**
+     * Anulabilidad del propio tipo de función
+     */
     // La propia variable puede almacenar una función o el valor null (pero la función no devuelve null)
     var funOrNull: ((Int, Int) -> Int)? = null
     // funOrNull = { x, y -> if (x == y) x else null } // error, no podemos asignar una función que puede devolver null
 
+    /**
+     * Anulabilidad de ambas
+     */
     // Tanto la variable puede valer null cono tener asignada una función que devuelve null
     var funCanReturnNullOrNull: ((Int, Int) -> Int?)? = null
     funCanReturnNullOrNull = {x,y -> if (x > y) x else null }
@@ -34,6 +50,7 @@ fun xxxx() {
 
 fun main() {
     explicitLambdaParameterTypesDeclaration()
-
+    implicitLambdaParameterTypesDeclaration()
+    nullability()
 }
 
