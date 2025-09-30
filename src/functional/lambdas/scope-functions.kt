c2b196df13694fd8d26eb1660b68ba2e9b9241cd
Rafa Puig <rafapuig.profe@gmail.com>
2025-09-30 14:05:37 +0200

    arreglos 30 sep v2

---
+++ src/functional/lambdas/scope-functions.kt
@@ -0,0 +1,16 @@
+package functional.lambdas
+
+/**
+ * La biblioteca estándar de Kotlin define 5 funciones scope
+ *
+ * let, run, with, apply y also
+ *
+ * Algunas son funciones de extensión
+ * otras son globales
+ *
+ * Algunas devuelven el resultado de la lambda
+ * otras (si son de extensión) devuelven el receptor
+ *
+ * Algunas usan una lambda regular
+ * otras una lambda con receptor
+ */
